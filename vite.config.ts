24b3bccf9bb65992958c6816eab35371088713f8
Cable-Network-School <kelebogile@tsalosesha.com>
2026-03-10 09:04:20 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,17 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '');
+    const env = loadEnv(mode, '.', '');
     return {
+        base: '/Projects/', 
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
-        base: '/', 
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
